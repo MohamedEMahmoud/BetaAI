e00c94c44d7870f54785e3ec96455c779037fada
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-28 18:40:46 +0200

    BetaAI

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 require("dotenv").config();
 const app = require("express")(),
-  bodyParser = require("body-parser"),
-  cors = require("cors"),
-  server = require("http").createServer(app),
-  io = require("socket.io")(server, { transports:['websocket']}),
-  cookieParser = require("cookie-parser"),
-  morgan = require("morgan"),
-  helmet = require("helmet"),
-  compression = require("compression");
+    bodyParser = require("body-parser"),
+    cors = require("cors"),
+    server = require("http").createServer(app),
+    io = require("socket.io")(server, { transports: ["websocket"] }),
+    cookieParser = require("cookie-parser"),
+    morgan = require("morgan"),
+    helmet = require("helmet"),
+    compression = require("compression");
 let whitelist = [
-  "https://beta-ai.vercel.app",
-  "http://localhost:3000",
-  "https://web.postman.co",
-  "https://betaai.tech",
-  "https://www.betaai.tech",
-  "http://localhost:4200",
-
+    "https://beta-ai.vercel.app",
+    "http://localhost:3000",
+    "https://web.postman.co",
+    "https://betaai.tech",
+    "https://www.betaai.tech",
+    "http://localhost:4200",
 ];
 let corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 // connect to db
@@ -33,62 +32,61 @@ const db = require(__dirname + "/models");
 const Role = db.role;
 
 db.mongoose
-  .connect(
-    process.env.NODE_ENV === "production"
-      ? `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DBSERVER}/${process.env.DB}`
-      : `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DBSERVER}/${process.env.DB}_test`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log(
-      `Successfully connect to MongoDB in ${process.env.NODE_ENV} mode`
-    );
-    initial();
-  })
-  .catch((err) => {
-    console.error("Connection error", err);
-    process.exit();
-  });
+    .connect(
+        process.env.NODE_ENV === "production" ?
+        `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DBSERVER}/${process.env.DB}` :
+        `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DBSERVER}/${process.env.DB}_test`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log(
+            `Successfully connect to MongoDB in ${process.env.NODE_ENV} mode`
+        );
+        initial();
+    })
+    .catch((err) => {
+        console.error("Connection error", err);
+        process.exit();
+    });
 
 const initial = () => {
-  Role.estimatedDocumentCount(async (err, count) => {
-    try {
-      if (!err && count === 0) {
-        const document = ["user", "instructor", "admin", "super", "recruiter"];
-        for (let i in document) {
-          await new Role({ name: document[i] }).save();
-          console.log(`added ${document[i]} to roles collection`);
+    Role.estimatedDocumentCount(async(err, count) => {
+        try {
+            if (!err && count === 0) {
+                const document = ["user", "instructor", "admin", "super", "recruiter"];
+                for (let i in document) {
+                    await new Role({ name: document[i] }).save();
+                    console.log(`added ${document[i]} to roles collection`);
+                }
+            }
+        } catch (e) {
+            console.log(e);
         }
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  });
+    });
 };
 
 app.use([
-  /** request type configuration **/
-  // parse requests of content-type - application/json
-  bodyParser.json(),
-  // parse requests of content-type - application/x-www-form-urlencoded
-  bodyParser.urlencoded({ extended: false }),
-  // morgan show requests
-  morgan("dev"),
-  cors(corsOptions),
-  cookieParser(),
-  helmet(),
-  compression(),
+    /** request type configuration **/
+    // parse requests of content-type - application/json
+    bodyParser.json(),
+    // parse requests of content-type - application/x-www-form-urlencoded
+    bodyParser.urlencoded({ extended: false }),
+    // morgan show requests
+    morgan("dev"),
+    cors(corsOptions),
+    cookieParser(),
+    helmet(),
+    compression(),
 ]);
 
 // cloudinary config
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_CLOUD_KEY,
-  api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_CLOUD_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
 });
 
 // start server
@@ -96,20 +94,20 @@ const PORT = process.env.PORT || 8088;
 server.listen(PORT, () => console.log("Beta AI listening on port 8088"));
 
 // require routes
-require(__dirname + "/modules/auth")(app);
-require(__dirname + "/modules/student")(app);
-require(__dirname + "/modules/course")(app);
-require(__dirname + "/modules/instructor")(app);
-require(__dirname + "/modules/compiler")(app);
-require(__dirname + "/modules/coupon")(app);
-require(__dirname + "/modules/cart")(app);
-require(__dirname + "/modules/wishlist")(app);
-require(__dirname + "/modules/checkout")(app);
-require(__dirname + "/modules/admin")(app);
-require(__dirname + "/modules/recruiter")(app);
-require(__dirname + "/modules/notification")(app);
-require(__dirname + "/modules/super")(app);
-require(__dirname + "/modules/chat")(app);
+require(__dirname + "/auth")(app);
+require(__dirname + "/student")(app);
+require(__dirname + "/course")(app);
+require(__dirname + "/instructor")(app);
+require(__dirname + "/compiler")(app);
+require(__dirname + "/coupon")(app);
+require(__dirname + "/cart")(app);
+require(__dirname + "/wishlist")(app);
+require(__dirname + "/checkout")(app);
+require(__dirname + "/admin")(app);
+require(__dirname + "/recruiter")(app);
+require(__dirname + "/notification")(app);
+require(__dirname + "/super")(app);
+require(__dirname + "/chat")(app);
 require(__dirname + "/services/loggers.services");
 
 // require sockets
@@ -124,17 +122,17 @@ require(__dirname + "/modules/chat/sockets/seen.socket")(io);
 
 // initial route
 app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ app: "betaAI-admin-core", core: "1.0.0", success: true });
+    res
+        .status(200)
+        .json({ app: "betaAI-admin-core", core: "1.0.0", success: true });
 });
 
 // // middleware for handling invalid paths
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({ message: "required path not found", status: 404, success: false });
-  next();
+    res
+        .status(404)
+        .json({ message: "required path not found", status: 404, success: false });
+    next();
 });
 
 module.exports = app;
